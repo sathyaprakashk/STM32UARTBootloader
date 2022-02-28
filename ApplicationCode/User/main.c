ca52cdc331a9c513e8f8fa222cea11e1047c9747
SathyaprakashK <sathyaprakashk@vayavyalabs.com>
2022-02-28 12:59:01 +0530

    fixed alignment

--- ApplicationCode/User/main.c
+++ ApplicationCode/User/main.c
@@ -59,12 +59,12 @@ int main(void)
 
 	  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 
-  /* Configure PA6 and PA7 in output pushpull mode */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6|GPIO_Pin_7;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	/* Configure PA6 and PA7 in output pushpull mode */
+  	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6|GPIO_Pin_7;
+  	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOA,&GPIO_InitStructure);
 	TimerConfig();		//Timer1 is configured this timer is used in 1ms delay generation function(Delay_ms())
 	while(1)
